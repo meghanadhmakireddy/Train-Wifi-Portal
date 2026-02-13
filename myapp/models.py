f78b8b869fb8cf600767c4bc55b1cc6f1c103862
meghanadhmakireddy <vamsimakireddy.842@gmail.com>
2026-02-13 15:02:06 +0530

    Added database of the passengers into the database

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TrainPassenger(models.Model):
+    pnr = models.CharField(max_length=10, unique=True)
+    mobile_number = models.CharField(max_length=10)
+    passenger_count = models.IntegerField()
+    journey_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.pnr
